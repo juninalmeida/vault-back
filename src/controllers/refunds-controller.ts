65cc8551ad3bfd554c8dadb81097f72ebd07cd06
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-22 17:24:16 -0300

    feat: add Zod validation to refund creation endpoint

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+
+const CategoriesEnum = z.enum([
+  'food',
+  'others',
+  'services',
+  'transport',
+  'accommodation',
+])
 
 class RefundsController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: 'Name is required' }),
+      category: CategoriesEnum,
+      amount: z
+        .number()
+        .positive({ message: 'amount must be a positive number' }),
+      filename: z.string().min(20),
+    })
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body)
+
     response.json({ message: 'Refund created successfully' })
   }
 }
