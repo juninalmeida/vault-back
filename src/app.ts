3612699f808bc02d84a715d07873576833749602
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-22 13:58:15 -0300

    feat: add error handling middleware with CORS and validation support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
+
+import { errorHandling } from './middlewares/error-handling'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
-app.get('/', (_req, res) => {
-  res.send('Hello, VaultBack!')
-})
+app.use(errorHandling)
 
 export { app }
