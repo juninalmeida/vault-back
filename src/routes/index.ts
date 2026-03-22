5adea061bb49bf8dd9eca93344452ae4d5859735
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-22 16:27:00 -0300

    feat: add refunds route and controller scaffold

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { usersRoutes } from '@/routes/users-routes'
+import { refundsRoutes } from '@/routes/refunds-routes'
 import { sessionsRoutes } from '@/routes/sessions-routes'
 
 const routes = Router()
@@ -9,5 +10,8 @@ const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/sessions', sessionsRoutes)
+
+// Rotas Privadas
+routes.use('/refunds', require('./refunds-routes').refundsRoutes)
 
 export { routes }
